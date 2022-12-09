c8113fd4308fe207f0fca3e1432d56f9fe46d3e0
Maiara Meneses <52250674+wildestmaii@users.noreply.github.com>
2022-12-09 03:33:15 -0300

    atualizações no banco de dados

---
+++ Banco de dados/Scripts - SQL/DDL_anderson_gas-Destruicao.sql
@@ -0,0 +1,59 @@
+-- -----------------------------------------------------
+-- Destruindo dados das tabelas
+-- -----------------------------------------------------
+
+TRUNCATE TABLE `andersonGas`.`endereco`;
+
+TRUNCATE TABLE `andersonGas`.`clientes`;
+
+TRUNCATE TABLE `andersonGas`.`inventario`;
+
+TRUNCATE TABLE `andersonGas`.`pedidos`;
+
+
+
+-- -----------------------------------------------------
+-- Destruindo tabelas
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS `andersonGas`.`endereco`;
+
+DROP TABLE IF EXISTS `andersonGas`.`clientes`;
+
+DROP TABLE IF EXISTS `andersonGas`.`inventario`;
+
+DROP TABLE IF EXISTS `andersonGas`.`pedidos`;
+
+
+
+-- -----------------------------------------------------
+-- Destruindo views
+-- -----------------------------------------------------
+
+DROP VIEW IF EXISTS `andersonGas`.
+
+DROP VIEW IF EXISTS `andersonGas`.
+
+DROP VIEW IF EXISTS `andersonGas`.
+
+DROP VIEW IF EXISTS `andersonGas`.
+
+DROP VIEW IF EXISTS `andersonGas`.
+
+DROP VIEW IF EXISTS `andersonGas`.
+
+DROP VIEW IF EXISTS `andersonGas`.
+
+DROP VIEW IF EXISTS `andersonGas`.
+
+DROP VIEW IF EXISTS `andersonGas`.
+
+DROP VIEW IF EXISTS `andersonGas`.
+
+
+
+-- -----------------------------------------------------
+-- Destruindo banco de dados
+-- -----------------------------------------------------
+
+DROP DATABASE  IF EXISTS andersonGas;
---
+++ Banco de dados/Scripts - SQL/DDL_anderson_gas-views.sql
@@ -0,0 +1,24 @@
+-- -----------------------------------------------------
+-- Criando Views
+-- -----------------------------------------------------
+
+CREATE VIEW IF NOT EXISTS 
+
+CREATE VIEW IF NOT EXISTS 
+
+CREATE VIEW IF NOT EXISTS 
+
+CREATE VIEW IF NOT EXISTS 
+
+CREATE VIEW IF NOT EXISTS 
+
+CREATE VIEW IF NOT EXISTS 
+
+CREATE VIEW IF NOT EXISTS 
+
+CREATE VIEW IF NOT EXISTS 
+
+CREATE VIEW IF NOT EXISTS 
+
+CREATE VIEW IF NOT EXISTS 
+
