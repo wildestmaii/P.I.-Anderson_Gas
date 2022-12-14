02df239e91783acc99c2d71e6436a12986c25481
Maiara Meneses <52250674+wildestmaii@users.noreply.github.com>
2022-12-14 07:09:15 -0300

    atualização DQL e DDL(pft)

--- Banco de dados/Scripts - SQL/DDL_anderson_gas-pft.sql
+++ Banco de dados/Scripts - SQL/DDL_anderson_gas-pft.sql
@@ -2,11 +2,58 @@
 -- Criação de procedures
 -- -----------------------------------------------------
 
+/*
+recebe dois parâmetros de entrada: inicio e fim, que representam as datas inicial e final, 
+respectivamente. Quando a procedure é chamada, ela retorna todos os pedidos realizados 
+entre as duas datas fornecidas
+*/
+
+CREATE PROCEDURE andersonGas.listar_pedidos(IN inicio DATE, IN fim DATE)
+BEGIN
+    SELECT * FROM andersonGas.pedidos
+    WHERE data_pedido BETWEEN inicio AND fim;
+END;
+
+/*
+cadastrar pedidos 
+*/
+
+CREATE PROCEDURE andersonGas.cadastrar_pedido(
+    IN p_data_pedido DATE,
+    IN p_nome_cliente VARCHAR(100),
+    IN p_telefone_cliente VARCHAR(9),
+    IN p_id_endereco INT,
+    IN p_nome_produto VARCHAR(45)
+)
+BEGIN
+    INSERT INTO andersonGas.clientes (nome, telefone, endereco_id_endereco)
+    VALUES (p_nome_cliente, p_telefone_cliente, p_id_endereco);
+
+    INSERT INTO andersonGas.pedidos (data_pedido, clientes_nome, clientes_endereco_id_endereco, inventario_nome_produto)
+    VALUES (p_data_pedido, p_nome_cliente, p_id_endereco, p_nome_produto);
+END;
 
 
 
+CREATE PROCEDURE 
 
+CREATE PROCEDURE 
 
+CREATE PROCEDURE 
+
+CREATE PROCEDURE 
+
+CREATE PROCEDURE 
+
+CREATE PROCEDURE 
+
+CREATE PROCEDURE 
+
+CREATE PROCEDURE 
+
+CREATE PROCEDURE 
+
+CREATE PROCEDURE 
 
 -- -----------------------------------------------------
 -- Criação de functions
@@ -21,6 +68,75 @@
 -- -----------------------------------------------------
 -- Criação de triggers
 -- -----------------------------------------------------
+
+CREATE TRIGGER nome_do_gatilho
+AFTER INSERT ON nome_da_tabela
+FOR EACH ROW
+BEGIN
+   -- aqui vão as instruções SQL que o gatilho irá executar
+END;
+
+
+/*
+para atualizar o preço de venda automaticamente
+*/
+
+CREATE TRIGGER atualizar_preco_venda
+AFTER UPDATE ON inventario
+FOR EACH ROW
+BEGIN
+   SET NEW.preco_de_venda = NEW.preco_de_compra * 1.2;
+END;
+
+
+/*
+verificar se a quantidade de produtos em estoque é suficiente para atender ao pedido antes 
+de inserir o pedido na tabela pedidos.
+*/
+
+DELIMITER //
+
+CREATE TRIGGER andersonGas.verifica_estoque_antes_insercao_pedido
+BEFORE INSERT ON andersonGas.pedidos
+FOR EACH ROW
+BEGIN
+    DECLARE quantidade_estoque INT;
+
+    SELECT quantidade INTO quantidade_estoque
+    FROM andersonGas.inventario
+    WHERE nome_produto = NEW.inventario_nome_produto;
+
+    IF quantidade_estoque < 1 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Estoque insuficiente para atender ao pedido';
+    END IF;
+END //
+
+DELIMITER ;
+
+
+
+/*
+manter o estoque do inventário atualizado sempre que um novo pedido é inserido na tabela pedidos
+*/
+
+CREATE TRIGGER andersonGas.atualizar_estoque
+AFTER INSERT ON andersonGas.pedidos
+FOR EACH ROW
+BEGIN
+    UPDATE andersonGas.inventario
+    SET quantidade = quantidade - 1
+    WHERE nome_produto = NEW.inventario_nome_produto;
+END;
+
+CREATE TRIGGER andersonGas.atualizar_quantidade_inventario
+AFTER INSERT ON andersonGas.pedidos
+FOR EACH ROW
+BEGIN
+    UPDATE andersonGas.inventario
+    SET quantidade = quantidade - 1
+    WHERE nome_produto = NEW.inventario_nome_produto;
+END;
 
 
 
--- Banco de dados/Scripts - SQL/DQL_anderson_anderson_gas.sql
+++ Banco de dados/Scripts - SQL/DQL_anderson_anderson_gas.sql
@@ -3,31 +3,138 @@
 -- -----------------------------------------------------
 
 
---------  listar bairros que mais solicitam, mostrando id_endereco, nome_cliente, nome_produto
+/* 
+listar pedidos feitos por clientes no bairro de casa amarela mostrando bairro, 
+cliente e nome do produto
+*/
+SELECT * FROM pedidos
+WHERE clientes_nome IN (
+   SELECT nome FROM clientes
+   WHERE endereco_id_endereco IN (
+      SELECT id_endereco FROM endereco
+      WHERE bairro = "Casa Amarela"
+   )
+);
 
 
--------- mostrar lucro do dia X listrando nome_produto
+/* 
+listar clientes e seus endereçoes
+*/
+
+SELECT c.nome, c.telefone, e.id_endereco, e.bairro, e.rua
+    FROM clientes AS c
+        INNER JOIN endereco AS e
+            ON c.endereco_id_endereco = e.id_endereco
+    ORDER BY e.bairro;
 
 
--------- mostrar quantidade vendida(subselect)
+
+SELECT nome,  bairro, rua, telefone
+FROM andersonGas.clientes
+INNER JOIN andersonGas.endereco
+ON clientes.endereco_id_endereco = endereco.id_endereco
 
 
---------
+
+/* 
+listar os produtos do inventário que foram pedidos pelos clientes pelo menos uma vez
+*/
+
+SELECT i.nome_produto, i.preco_de_venda, i.quantidade
+    FROM inventario AS i
+        INNER JOIN pedidos AS p 
+			ON i.nome_produto = p.inventario_nome_produto
+    GROUP BY i.nome_produto
+    HAVING COUNT(p.id_pedido) > 0;
 
 
---------
+/* 
+listar todos os pedidos que foram realizados por clientes com nome "Silva"
+*/
+
+SELECT *
+    FROM pedidos AS p
+    WHERE p.clientes_nome IN 
+        (SELECT nome
+            FROM clientes
+            WHERE nome LIKE "%Silva%");
 
 
---------
+/* 
+listar clientes que tenham feito pelo pedidos em uma determinada data
+*/
+
+SELECT *
+FROM clientes
+WHERE EXISTS (
+    SELECT *
+		FROM pedidos
+		WHERE pedidos.clientes_nome = clientes.nome
+			AND pedidos.clientes_endereco_id_endereco = clientes.endereco_id_endereco
+			AND pedidos.data_pedido = '01-12-22'
+)
 
 
---------  listar clientes q maks realozaram pedidos mostrando nome_cliente, num_telefone, id_endereco
+/* 
+lista de todos os produtos cuja diferença entre o preço de compra e o preço de 
+venda seja maior que 10
+*/
+
+SELECT nome_produto, preco_de_compra, preco_de_venda, (preco_de_venda - preco_de_compra) AS lucro
+    FROM inventario
+    WHERE ABS(preco_de_venda - preco_de_compra) > 10
 
 
---------
+/* 
+retornar informações dos clientes, endereços, pedidos e produtos, além de 
+calcular o lucro obtido em cada pedido, com base na diferença entre o preço de venda 
+e o preço de compra dos produtos.
+*/
+
+SELECT clientes.nome, clientes.telefone, endereco.cidade, endereco.UF, pedidos.data_pedido, 
+       inventario.nome_produto, inventario.preco_de_compra, inventario.preco_de_venda,
+       (inventario.preco_de_venda - inventario.preco_de_compra) AS lucro
+FROM pedidos
+JOIN clientes ON pedidos.clientes_nome = clientes.nome
+JOIN endereco ON clientes.endereco_id_endereco = endereco.id_endereco
+JOIN inventario ON pedidos.inventario_nome_produto = inventario.nome_produto
 
 
---------
+/*
+retornar os pedidos realizados por clientes de determinada cidade, junto com o 
+preço de venda dos produtos
+*/
+
+SELECT p.id_pedido, c.nome AS cliente, i.preco_de_venda
+    FROM andersonGas.pedidos AS p
+        JOIN andersonGas.clientes AS c
+        ON p.clientes_nome = c.nome AND p.clientes_endereco_id_endereco = c.endereco_id_endereco
+        JOIN andersonGas.inventario AS i
+        ON p.inventario_nome_produto = i.nome_produto
+        JOIN andersonGas.endereco AS e
+        ON c.endereco_id_endereco = e.id_endereco
+        WHERE e.cidade = 'Recife';
 
 
---------
+/*
+
+*/
+
+SELECT c.nome AS nome_cliente, c.telefone, e.CEP, e.cidade, e.bairro, e.rua, e.numero, e.UF
+    FROM andersonGas.clientes AS c
+    JOIN andersonGas.endereco AS e ON c.endereco_id_endereco = e.id_endereco;
+
+
+
+
+/*
+retornar as informações d
+*/
+
+
+SELECT clientes_nome, inventario_nome_produto, data_pedido
+    FROM pedidos
+    WHERE pedidos.data_pedido BETWEEN '2022-01-01' AND '2022-12-31';
+
+
+
